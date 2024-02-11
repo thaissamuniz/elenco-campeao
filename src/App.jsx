d34cd424f0b491d500515f6c54a7e9a1481b8c4d
thaissamuniz <thaiissa.muniiz@gmail.com>
2024-02-10 21:10:10 -0300

    Revert "create home page component"
    This reverts commit 289bf9aa58ac4e65fffce333d135eb3bf02b224f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 import Header from './components/Header'
-import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <Home />
+      <Header />
     </>
   )
 }
